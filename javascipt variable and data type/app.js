de7d695111e37aefe33237d5233b26d2aff0614b
developerfahad1 <fahadgraphic.pk>
2025-05-16 12:32:28 +0500

    javascript varaible and data types

---
+++ javascipt variable and data type/app.js
@@ -0,0 +1,80 @@
+let myName = 'My name is Muhammad Fahad';
+console.log(myName);
+
+let mathMarks = 90;
+let englishMarks = 70;
+console.log('TotalMarks =' ,mathMarks + englishMarks);
+
+
+let isRatting = true;
+console.log('is Rating is ' , isRatting);
+
+const country = 'pakistan';
+// country = 'india';
+console.log(country);
+
+
+let x = null;
+let y ;
+console.log(x);
+console.log(y);
+
+
+console.log('javascript data types section start ');
+
+// let name = 'fahad' //string
+// console.log(typeof name);
+
+// let age = 20; //number
+// console.log(typeof age);
+
+// let isStudent = true; // boolean
+// console.log(typeof isStudent);
+
+// let emty = null; // null
+// console.log(typeof emty);
+
+// let fullname; // undefined
+// console.log(typeof fullname);
+
+
+// Exercise 1: String Type
+let greeting = 'Asalamwalikom';
+console.log(typeof greeting);
+
+
+// Exercise 2: Number Type
+let price = 199.99;
+console.log(typeof price);
+
+// Exercise 3: Boolean Type
+let isLoggedIn = false;
+console.log(typeof isLoggedIn);
+
+
+// Exercise 4: Null & Undefined
+let a = null;
+let b;
+console.log(typeof a);
+console.log(typeof b);
+
+// Exercise 5: Object & Array
+let age = {name: 'muhammadfahad', age: 20};
+console.log(typeof age);
+
+
+let fruite = ['apple', 'mango', 'banana'];
+console.log(typeof fruite);
+
+
+// varaible or datatype mini project 
+
+let productName = 'Smart watch';
+let productPrice = 200;
+let isAvailable = true;
+
+console.log('productName =',  productName );
+console.log('productprice =', productPrice);
+console.log('isAvailable =', isAvailable);
+
+
