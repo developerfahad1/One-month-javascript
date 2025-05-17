8ee82926afc984fce04b99c49a115fdb567fc810
developerfahad1 <fahadgraphic.pk>
2025-05-17 13:04:06 +0500

    javascipt varaible and datatype and operator add

--- javascipt variable and data type/app.js
+++ javascipt variable and data type/app.js
@@ -1,26 +1,26 @@
-let myName = 'My name is Muhammad Fahad';
-console.log(myName);
+// let myName = 'My name is Muhammad Fahad';
+// console.log(myName);
 
-let mathMarks = 90;
-let englishMarks = 70;
-console.log('TotalMarks =' ,mathMarks + englishMarks);
+// let mathMarks = 90;
+// let englishMarks = 70;
+// console.log('TotalMarks =' ,mathMarks + englishMarks);
 
 
-let isRatting = true;
-console.log('is Rating is ' , isRatting);
+// let isRatting = true;
+// console.log('is Rating is ' , isRatting);
 
-const country = 'pakistan';
+// const country = 'pakistan';
 // country = 'india';
-console.log(country);
+// console.log(country);
 
 
-let x = null;
-let y ;
-console.log(x);
-console.log(y);
+// let x = null;
+// let y ;
+// console.log(x);
+// console.log(y);
 
 
-console.log('javascript data types section start ');
+// console.log('javascript data types section start ');
 
 // let name = 'fahad' //string
 // console.log(typeof name);
@@ -39,42 +39,122 @@ console.log('javascript data types section start ');
 
 
 // Exercise 1: String Type
-let greeting = 'Asalamwalikom';
-console.log(typeof greeting);
+// let greeting = 'Asalamwalikom';
+// console.log(typeof greeting);
 
 
 // Exercise 2: Number Type
-let price = 199.99;
-console.log(typeof price);
+// let price = 199.99;
+// console.log(typeof price);
 
 // Exercise 3: Boolean Type
-let isLoggedIn = false;
-console.log(typeof isLoggedIn);
+// let isLoggedIn = false;
+// console.log(typeof isLoggedIn);
 
 
 // Exercise 4: Null & Undefined
-let a = null;
-let b;
-console.log(typeof a);
-console.log(typeof b);
+// let a = null;
+// let b;
+// console.log(typeof a);
+// console.log(typeof b);
 
 // Exercise 5: Object & Array
-let age = {name: 'muhammadfahad', age: 20};
-console.log(typeof age);
+// let age = {name: 'muhammadfahad', age: 20};
+// console.log(typeof age);
 
 
-let fruite = ['apple', 'mango', 'banana'];
-console.log(typeof fruite);
+// let fruite = ['apple', 'mango', 'banana'];
+// console.log(typeof fruite);
 
 
 // varaible or datatype mini project 
 
-let productName = 'Smart watch';
-let productPrice = 200;
-let isAvailable = true;
+// let productName = 'Smart watch';
+// let productPrice = 200;
+// let isAvailable = true;
 
-console.log('productName =',  productName );
-console.log('productprice =', productPrice);
-console.log('isAvailable =', isAvailable);
+// console.log('productName =',  productName );
+// console.log('productprice =', productPrice);
+// console.log('isAvailable =', isAvailable);
 
 
+
+// Operator in Javascript 
+
+
+// Arithematic operator 
+// four basic arthematic operator 
+
+
+// let a  = 5;
+// let b = 2;
+// console.log("a + b = ", a + b); // addition 
+// console.log("a - b = ", a - b); // subtraction
+// console.log("a * b = ", a * b); // multiplication
+// console.log("a / b = ", a / b); // division
+// console.log("a % b = ", a % b); // modulas ma jo reminder bacha ga wahe console ma show hoga 
+// console.log('a ** b =', a ** b); // exponention 
+
+// Unary Operator 
+
+// increment 
+// decrement 
+
+// increment method 
+// let a = 5;
+// let b = 2;
+
+// console.log('a + b =', a + b);
+// a = a + 1;
+// console.log('a =', a);
+
+
+// decrement method 
+// let a = 5;
+// let b = 2;
+
+// console.log('a + b =', a + b);
+// a = a - 1;
+// console.log(a);
+
+
+
+// Assignment Operator 
+
+// let a = 5;
+// let b = 2;
+
+// a += 4; // is code ka matlab ha ka a = a + 4 yane ka a ke value 5 ha or usma plus karo 4 ko tho console ma 9 hoga
+// console.log('a =', a);
+
+// same matlab - ka be ha 
+// a -= 4;
+// console.log('a =', a);
+
+// same matlab multiply ka leya be ha 
+// a *= 4;
+// console.log('a =', a);
+
+// a /= 4;
+// console.log('a =', a);
+
+
+
+
+
+// Comparison operator 
+
+// let a = 5;
+// let b = 2;
+
+// console.log('5 != 2', a != b);
+
+
+let age = 22;
+
+console.log(age > 18);
+
+
+let a = 10;
+let b = '10';
+console.log(a === b);
