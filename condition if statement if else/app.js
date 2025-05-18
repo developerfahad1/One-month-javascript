4fd678344444bf7faa2ce2a08091651963e672fd
developerfahad1 <fahadgraphic.pk>
2025-05-18 13:14:44 +0500

    condition if statement folder add

---
+++ condition if statement if else/app.js
@@ -0,0 +1,54 @@
+console.log('Muhammad fahad front end developer');
+
+// let age = 18;
+
+// if(age >= 18){
+//     console.log('you can vote');
+    
+// }
+
+
+let age = 19;
+
+if(age >= 18){
+    console.log('you can vote');
+    
+}else{
+    console.log('you can not vote');
+    
+}
+
+// multiple condition check example 
+
+let marks = 65;
+
+if(marks >= 90){
+    console.log('Grade : A');
+    
+} else if(marks >= 75){
+    console.log('Grade : B');
+    
+} else if(marks >= 60){
+    console.log('Grade : C');
+    
+} else if(marks <= 33){
+    console.log('you can fail');
+    
+}
+
+
+let Time = 12;
+
+if(Time <= 12){
+    console.log('Good Morning');
+    
+} else if(Time <= 18){
+    console.log('Good Afternoon');
+    
+} else if(Time <= 24){
+    console.log('Good Evening');
+    
+} else{
+    console.log('invailid time');
+    
+}
