fa0c1ea7d32212b00e6ca98ba51c05780225ffb3
developerfahad1 <fahadgraphic.pk>
2025-05-24 13:04:37 +0500

    switch statement in js add

---
+++ switch statement in JS/app.js
@@ -0,0 +1,53 @@
+console.log("Muhammad Fahad Frontent Developer");
+
+let day = 3;
+
+switch (day) {
+  case 0:
+    console.log("Monday");
+    break;
+  case 1:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("saturday");
+    break;
+  case 7:
+    console.log("sunday");
+    break;
+  default:
+    console.log("unknown day");
+}
+
+
+
+let Grade = 'a';
+
+switch (Grade) {
+  case 'a':
+    console.log("A Grade is Excellent");
+    break;
+  case 'b':
+    console.log("B Grade is Good");
+    break;
+  case 'c':
+    console.log("C Grade is Average");
+    break;
+  case 'd':
+    console.log("D need improvment");
+    break;
+  case 'f':
+    console.log("F is fail Try again");
+    break;
+  default:
+    console.log("unknown grade");
+}
